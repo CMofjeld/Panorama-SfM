ef722b74af9e2e20fe6cb90d4c73781d9692513c
Drew Nelson <DrewNelson1991@gmail.com>
2021-05-11 21:30:54 -0700

    first commit

---
+++ CSS_587_MofjeldNelson_Project/main.cpp
@@ -0,0 +1,69 @@
+/*
+* CSS 587 - Advance Computer Vision
+* Spring 2021
+* Carl Moffjeld, Drew Nelson
+* 
+* Description:
+* TODO: Add more description for the file when we're further along in the project
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <chrono>
+#include <ctime>   
+
+using namespace cv;
+using namespace std;
+
+/// <summary>
+/// Read a video from the local "Input" directory into memory.
+/// </summary>
+/// <param name="fileName">Full file path to the video to load</param>
+/// <param name="cap">[Output] Loaded video file</param>
+/// <returns>True if video loaded succesfully. False otherwise</returns>
+bool LoadVideo(String fileName, VideoCapture &cap)
+{
+    cap = VideoCapture("Input\\" + fileName);
+    bool videoLoaded = cap.isOpened();
+    if (cap.isOpened())
+    {
+        printf("Failed to read video at %s", fileName.c_str());
+    }
+    return videoLoaded;
+}
+
+/// <summary>
+/// Application entry point. 
+/// TODO: Add further description when we're further along in the project
+/// </summary>
+/// <returns></returns>
+int main()
+{
+    VideoCapture loadedVideo;
+    int exitValue = -1;
+    if (LoadVideo("short_test_vid.mp4", loadedVideo)) 
+    {
+        double fps = loadedVideo.get(CAP_PROP_FPS);
+        String windowName = "Test Video";
+
+        namedWindow("Test Video", WINDOW_AUTOSIZE);
+        while (loadedVideo.isOpened())
+        {
+            Mat frame;
+            // Check to see if we've reached the end or if the user has hit the escape key
+            if (!loadedVideo.read(frame) || waitKey(30) == 27)
+            {
+                break;
+            }
+            imshow(windowName, frame);
+        }
+        loadedVideo.release();
+        exitValue = 0;
+    }
+
+    printf("Program finished");
+    waitKey(0);
+    return exitValue;
+}
