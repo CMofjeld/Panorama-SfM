a55e49f1bedb59b33eee7416eba7d9feb9e9c8eb
CMofjeld <cmofjeld@uw.edu>
2021-06-05 14:49:24 -0700

    Removed duplicate solver implementations.

--- CSS_587_MofjeldNelson_Project/FundamentalSolvers.cpp
+++ CSS_587_MofjeldNelson_Project/FundamentalSolvers.cpp
@@ -10,83 +10,6 @@
 /// <param name="points1">List of four points in the first image</param>
 /// <param name="points2">List of four points in the second image</param>
 /// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
-/// <returns>Estimated fundamental matrix</returns>
-void fourPointMethod(const vector<Point2f>& points1, const vector<Point2f>& points2, vector<Mat>& solutions) {
-   // TODO: input validation
-   
-   // Set up system of linear equations based on the epipolar constraint
-   size_t num_correspondences = points1.size();
-   Mat A(num_correspondences, 6, CV_32FC1);
-   for (size_t i = 0; i < num_correspondences; i++)
-   {
-      A.at<float>(i, 0) = points1[i].x * points2[i].x - points1[i].y * points2[i].y;
-      A.at<float>(i, 1) = points1[i].x * points2[i].y + points1[i].y * points2[i].x;
-      A.at<float>(i, 2) = points2[i].x;
-      A.at<float>(i, 3) = points2[i].y;
-      A.at<float>(i, 4) = points1[i].x;
-      A.at<float>(i, 5) = points1[i].y;
-   }
-
-   // Find a basis for the nullspace (and the vectorized fundamental matrix, Fv)
-   SVD svd(A, SVD::FULL_UV);  // FULL_UV flag needed to get nullspace basis vectors
-
-   // Fv is a combination of the nullspace vectors according to the equation:
-   // Fv = x*F1 + (1-x)*F2
-   // where x is scalar determining the relative proportion of each nullspace vector.
-   Vec6f f1 = svd.vt.row(4); // first nullspace basis vector
-   Vec6f f2 = svd.vt.row(5); // second nullspace basis vector
-
-   vector<Mat> Fmats(2);   // fundamental matrices generated from the basis vectors
-   Fmats[0] = (Mat_<float>(3, 3) << f1(0), f1(1),  f1(4),
-               f1(1), -f1(0), f1(5),
-               f1(2), f1(3),  0);
-   Fmats[1] = (Mat_<float>(3, 3) << f2(0), f2(1),  f2(4),
-               f2(1), -f2(0), f2(5),
-               f2(2), f2(3),  0);
-
-   // The fundamental matrix must have a zero determinant.
-   // Set up a cubic equation based on this to find x.
-   // Reference for this code:
-   // https://imkaywu.github.io/blog/2017/06/fundamental-matrix/
-   float D[2][2][2]; // intermediate determinants
-   for (size_t i1 = 0; i1 < 2; i1++)
-   {
-      for (size_t i2 = 0; i2 < 2; i2++)
-      {
-         for (size_t i3 = 0; i3 < 2; i3++)
-         {
-            Mat_<float> Dtmp(3,3);
-            Fmats[i1].col(0).copyTo(Dtmp.col(0));
-            Fmats[i2].col(1).copyTo(Dtmp.col(1));
-            Fmats[i3].col(2).copyTo(Dtmp.col(2));
-            D[i1][i2][i3] = determinant(Dtmp);
-         }
-      }
-   }
-   Vec4f coefficients(4);  // coefficients of the cubic equation
-   coefficients(0) = -D[1][0][0] + D[0][1][1] + D[0][0][0] + D[1][1][0] + D[1][0][1] - D[0][1][0] - D[0][0][1] - D[1][1][1];
-   coefficients(1) = D[0][0][1] - 2 * D[0][1][1] - 2 * D[1][0][1] + D[1][0][0] - 2 * D[1][1][0] + D[0][1][0] + 3 * D[1][1][1];
-   coefficients(2) = D[1][1][0] + D[0][1][1] + D[1][0][1] - 3 * D[1][1][1];
-   coefficients(3) = D[1][1][1];
-   Vec3f roots;    // solutions to the cubic equation
-   int numRoots = solveCubic(coefficients, roots);
-
-   // The cubic equation may have up to 3 possible solutions for x.
-   // Reconstruct a possible fundamental matrix solution for each x
-   // and store it in the output vector.
-   for (size_t i = 0; i < numRoots; i++)
-   {
-      Mat solution = Fmats[0] * roots[i] + Fmats[1] * (1.f - roots[i]);
-      solutions.push_back(solution.t() / norm(solution));
-   }
-}
-
-/// <summary>
-/// Estimate the fundamental matrix between two images using four point correspondences.
-/// </summary>
-/// <param name="points1">List of four points in the first image</param>
-/// <param name="points2">List of four points in the second image</param>
-/// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
 void fourPointMethod(const Mat& points1, const Mat& points2, vector<Mat>& solutions) {
    // TODO: input validation
 
@@ -157,22 +80,6 @@ void fourPointMethod(const Mat& points1, const Mat& points2, vector<Mat>& soluti
 /// <summary>
 /// Estimate the fundamental matrix between two images using seven point correspondences.
 /// </summary>
-/// <param name="points1">List of seven points in the first image</param>
-/// <param name="points2">List of seven points in the second image</param>
-/// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
-void sevenPointMethod(const vector<Point2f>& points1, const vector<Point2f>& points2, vector<Mat>& solutions) {
-   Mat solver_results = findFundamentalMat(points1, points2, FM_7POINT);
-   solver_results.convertTo(solver_results, CV_32F);
-   for (size_t i = 0; i < solver_results.rows; i += 3)
-   {
-      Mat single_result = solver_results(Rect(0, i, 3, 3));
-      solutions.push_back(single_result / norm(single_result));
-   }
-}
-
-/// <summary>
-/// Estimate the fundamental matrix between two images using seven point correspondences.
-/// </summary>
 /// <param name="points1">Nx3 matrix of homogeneous points in the first image</param>
 /// <param name="points2">Nx3 matrix of homogeneous points in the second image</param>
 /// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
@@ -189,22 +96,6 @@ void sevenPointMethod(const Mat& points1, const Mat& points2, vector<Mat>& solut
 /// <summary>
 /// Estimate the fundamental matrix between two images using eight point correspondences.
 /// </summary>
-/// <param name="points1">List of seven points in the first image</param>
-/// <param name="points2">List of seven points in the second image</param>
-/// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
-void eightPointMethod(const vector<Point2f>& points1, const vector<Point2f>& points2, vector<Mat>& solutions) {
-    Mat solver_results = findFundamentalMat(points1, points2, FM_8POINT);
-    solver_results.convertTo(solver_results, CV_32F);
-    for (size_t i = 0; i < solver_results.rows; i += 3)
-    {
-        Mat single_result = solver_results(Rect(0, i, 3, 3));
-        solutions.push_back(single_result / norm(single_result));
-    }
-}
-
-/// <summary>
-/// Estimate the fundamental matrix between two images using eight point correspondences.
-/// </summary>
 /// <param name="points1">Nx3 matrix of homogeneous points in the first image</param>
 /// <param name="points2">Nx3 matrix of homogeneous points in the second image</param>
 /// <param name="solutions">[Output] Possible solutions for the fundamental matrix</param>
@@ -216,62 +107,6 @@ void eightPointMethod(const Mat& points1, const Mat& points2, vector<Mat>& solut
         Mat single_result = solver_results(Rect(0, i, 3, 3));
         solutions.push_back(single_result / norm(single_result));
     }
-}
-
-/// <summary>
-/// Estimate the fundamental matrix robustly with RANSAC and the four point method.
-/// </summary>
-/// <param name="points1">List of points in the first image</param>
-/// <param name="points2">List of points in the second image</param>
-/// <param name="iterations">Maximum number of iterations</param>
-/// <param name="threshold">Error threshold for inlier/outlier calculation</param>
-/// <returns>Estimated fundamental matrix</returns>
-Mat estimateFundamentalMatrix(const vector<Point2f>& points1, const vector<Point2f>& points2, int iterations, float threshold)
-{
-   // TODO: input validation
-   Mat bestEstimate;       // Best estimate for fundamental matrix
-   int bestInliers = -1;   // Number of inliers for best estimate
-   static RNG rng(12345);  // Random number generator for selecting random subsets
-   int minIndex = 0;                // Minimum subset index
-   int maxIndex = points1.size();   // Maximum subset index
-   const int SUBSAMPLE_SIZE = 4;    // Minimum points for estimating fundamental
-
-   // For specified iterations:
-   for (int i = 0; i < iterations; i++)
-   {
-      // Select random subsample of 4
-      vector<int> indices;
-      unordered_set<int> previousIndices;
-      for (size_t i = 0; i < SUBSAMPLE_SIZE; i++)
-      {
-         int newRandomIndex;
-         do
-         {
-            newRandomIndex = rng.uniform(minIndex, maxIndex);
-         } while (previousIndices.count(newRandomIndex) > 0);
-         indices.push_back(newRandomIndex);
-      }
-      vector<Point2f> subsample1, subsample2;
-      for (auto& index : indices) {
-         subsample1.push_back(points1[index]);
-         subsample2.push_back(points2[index]);
-      }
-
-      // Get potential solutions
-      vector<Mat> solutions;
-      fourPointMethod(subsample1, subsample2, solutions);
-
-      // Check for new best estimate
-      for (auto& solution : solutions) {
-         int curInliers = countInliersFundamental(solution, points1, points2, threshold);
-         if (curInliers > bestInliers) {
-            bestEstimate = solution;
-            bestInliers = curInliers;
-         }
-      }
-   }
-   // Return best estimate
-   return bestEstimate;
 }
 
 /// <summary>
@@ -343,35 +178,6 @@ Mat estimateFundamentalMatrix(CustomSolver solver, const Mat& points1, const Mat
 /// Fundamental matrix and a threshold for error in the epipolar constraint.
 /// </summary>
 /// <param name="F">Fundamental matrix</param>
-/// <param name="points1">List of points in the first image</param>
-/// <param name="points2">List of points in the second image</param>
-/// <param name="threshold">Error threshold for inlier/outlier calculation</param>
-/// <returns>Number of inliers</returns>
-int countInliersFundamental(const Mat& F, const vector<Point2f>& points1, const vector<Point2f>& points2, float threshold) {
-   // Convert to matrices for quicker calculation
-   Mat homogeneousP1, homogeneousP2;
-   vector<Point2f> points1copy(points1), points2copy(points2);
-   hconcat(Mat(points1copy.size(), 2, CV_32F, points1copy.data()), Mat::ones(points1copy.size(), 1, CV_32F), homogeneousP1);
-   hconcat(Mat(points2copy.size(), 2, CV_32F, points2copy.data()), Mat::ones(points2copy.size(), 1, CV_32F), homogeneousP2);
-   
-   // Get epipolar lines
-   Mat epipolarLines = homogeneousP2 * F;
-
-   // Count inliers
-   int inliers = 0;
-   for (size_t i = 0; i < epipolarLines.rows; i++)
-   {
-      double error = homogeneousP1.row(i).dot(epipolarLines.row(i));
-      if (error < threshold) ++inliers;
-   }
-   return inliers;
-}
-
-/// <summary>
-/// Count the number of inliers in a set of point correspondences based on a given
-/// Fundamental matrix and a threshold for error in the epipolar constraint.
-/// </summary>
-/// <param name="F">Fundamental matrix</param>
 /// <param name="points1">Nx3 matrix of homogeneous points in the first image</param>
 /// <param name="points2">Nx3 matrix of homogeneous points in the second image</param>
 /// <param name="threshold">Error threshold for inlier/outlier calculation</param>
@@ -428,7 +234,7 @@ Mat fundamentalFromImagePair(const Mat& img1, const Mat& img2) {
    hconcat(Mat(matches2.size(), 2, CV_32F, matches2.data()), Mat::ones(matches2.size(), 1, CV_32F), homogeneousP2);
 
    // Estimate the fundamental matrix
-   return estimateFundamentalMatrix(homogeneousP1, homogeneousP2);
+   return estimateFundamentalMatrix(FourPointSolver, homogeneousP1, homogeneousP2);
 }
 
 /// <summary>
